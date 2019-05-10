1bef0a9c0aacab4beff8f90bd097626404304a4a
GrimmOutlook <grimmoutlook87@gmail.com>
2019-05-10 19:08:16 -0400

    Added form to RSVP component.

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -5,6 +5,37 @@ import './RSVP.css';
 const RSVP = () => (
 	<section id="rsvp" className="container__rsvp">
 		<h1>RSVP</h1>
+		<form name="contact" netlify>
+			<p>Please RSVP by August 1st</p>
+			<fieldset>
+				<legend>Will You Be Attending?</legend>
+				<input type="radio" name="rsvp" id="yes" value="yes" />
+				<label htmlFor="yes">Accept</label>
+				<input type="radio" name="rsvp" id="no" value="no" />
+				<label htmlFor="no">Decline</label>
+			</fieldset>
+			<fieldset>
+				<div>
+					<label>
+						First Name <input type="text" name="firstName" />
+					</label>
+				</div>
+				<div>
+					<label>
+						Last Name
+						<input type="text" name="lastName" />
+					</label>
+				</div>
+				<div>
+					<label>
+						Email <input type="email" name="email" />
+					</label>
+				</div>
+				<div>
+					<button type="submit">Submit</button>
+				</div>
+			</fieldset>
+		</form>
 	</section>
 );
 
