b14607b414bcaca655352a29125aa608e393c68e
GrimmOutlook <grimmoutlook87@gmail.com>
2019-05-17 17:32:57 -0400

    Much work on RSVP form for mobile screens.

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -8,32 +8,53 @@ const RSVP = () => (
 		<form name="contact" method="POST" data-netlify="true">
 			<input type="hidden" name="form-name" value="contact" />
 			<p>Please RSVP by August 1st</p>
-			<fieldset>
+			<fieldset className="field__attend">
 				<legend>Will You Be Attending?</legend>
-				<input type="radio" name="rsvp" id="yes" value="yes" />
-				<label htmlFor="yes">Accept</label>
-				<input type="radio" name="rsvp" id="no" value="no" />
-				<label htmlFor="no">Decline</label>
+				<div className="attend__group">
+					<div className="input__attend">
+						<input type="radio" name="rsvp" id="yes" value="yes" />
+						<label htmlFor="yes">
+							<span className="attend__btn" />
+							Accept
+						</label>
+					</div>
+					<div className="input__attend">
+						<input type="radio" name="rsvp" id="no" value="no" />
+						<label htmlFor="no">
+							<span className="attend__btn" />
+							Decline
+						</label>
+					</div>
+				</div>
 			</fieldset>
-			<fieldset>
-				<div>
-					<label>
-						First Name <input type="text" name="firstName" />
-					</label>
+			<fieldset className="field__name">
+				<div className="input__name">
+					<label htmlFor="firstName">First Name:</label>
+					<input type="text" name="firstName" id="firstName" />
+				</div>
+				<div className="input__name">
+					<label htmlFor="lastName">Last Name:</label>
+					<input type="text" name="lastName" id="lastName" />
+				</div>
+				<div className="input__name">
+					<label htmlFor="email">Email:</label>
+					<input type="email" name="email" id="email" />
+				</div>
+			</fieldset>
+			<fieldset className="field__song">
+				<legend align="center">Request a song to play at the reception:</legend>
+				<div className="input__song">
+					<label htmlFor="song">Song:</label>
+					<input type="text" name="song" id="song" />
+				</div>
+				<div className="input__song">
+					<label htmlFor="artist">Artist:</label>
+					<input type="text" name="artist" id="artist" />
 				</div>
 				<div>
-					<label>
-						Last Name
-						<input type="text" name="lastName" />
-					</label>
-				</div>
-				<div>
-					<label>
-						Email <input type="email" name="email" />
-					</label>
-				</div>
-				<div>
-					<button type="submit">Submit</button>
+					<button type="submit" className="btn__submit">
+						Submit
+					</button>
 				</div>
 			</fieldset>
 		</form>
