82a1578736da586cfaa115ab4c4a7cafdc0170c9
GrimmOutlook <grimmoutlook87@gmail.com>
2019-05-20 13:26:12 -0400

    Attend radio buttons now working and looking right.

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -9,19 +9,19 @@ const RSVP = () => (
 			<input type="hidden" name="form-name" value="contact" />
 			<p>Please RSVP by August 1st</p>
 			<fieldset className="field__attend">
-				<legend>Will You Be Attending?</legend>
-				<div className="attend__group">
-					<div className="input__attend">
-						<input type="radio" name="rsvp" id="yes" value="yes" />
-						<label htmlFor="yes">
-							<span className="attend__btn" />
+				<legend className="legend__attend">Will You Be Attending?</legend>
+				<div className="form__group">
+					<div className="form__radio-group">
+						<label className="form__radio-label">
+							<input type="radio" className="form__radio-input" name="rsvp" id="yes" value="yes" />
+							<span className="form__radio-button" />
 							Accept
 						</label>
 					</div>
-					<div className="input__attend">
-						<input type="radio" name="rsvp" id="no" value="no" />
-						<label htmlFor="no">
-							<span className="attend__btn" />
+					<div className="form__radio-group">
+						<label className="form__radio-label">
+							<input type="radio" className="form__radio-input" name="rsvp" id="no" value="no" />
+							<span className="form__radio-button" />
 							Decline
 						</label>
 					</div>
@@ -30,26 +30,26 @@ const RSVP = () => (
 			<fieldset className="field__name">
 				<div className="input__name">
 					<label htmlFor="firstName">First Name:</label>
-					<input type="text" name="firstName" id="firstName" />
+					<input type="text" className="input__bar" name="firstName" id="firstName" />
 				</div>
 				<div className="input__name">
 					<label htmlFor="lastName">Last Name:</label>
-					<input type="text" name="lastName" id="lastName" />
+					<input type="text" className="input__bar" name="lastName" id="lastName" />
 				</div>
 				<div className="input__name">
 					<label htmlFor="email">Email:</label>
-					<input type="email" name="email" id="email" />
+					<input type="email" className="input__bar" name="email" id="email" />
 				</div>
 			</fieldset>
 			<fieldset className="field__song">
 				<legend align="center">Request a song to play at the reception:</legend>
 				<div className="input__song">
 					<label htmlFor="song">Song:</label>
-					<input type="text" name="song" id="song" />
+					<input type="text" className="input__bar" name="song" id="song" />
 				</div>
 				<div className="input__song">
 					<label htmlFor="artist">Artist:</label>
-					<input type="text" name="artist" id="artist" />
+					<input type="text" className="input__bar" name="artist" id="artist" />
 				</div>
 				<div>
 					<button type="submit" className="btn__submit">
