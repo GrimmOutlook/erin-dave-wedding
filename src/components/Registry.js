37aed6fe5a45a3cc2744edc975857c512bfce790
GrimmOutlook <grimmoutlook87@gmail.com>
2019-06-02 18:36:52 -0400

    Registry section completed.

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -10,12 +10,67 @@ const Registry = () => (
 			fund. Make your donation to any charity here:{' '}
 			<a href="https://thegoodbeginning.com/registries/david-grimm-erin-cantwell/">TheGoodBeginning.com</a>
 		</p>
-		<img src="/images/wwp_logo_black.png" alt="Wounded Warrior Project logo" className="logo__charity" />
-		<p className="registry__label">Wounded Warrior Project</p>
-		<img src="/images/make-logo.jpeg" alt="MAKE Studio logo" className="logo__charity" />
-		<p className="registry__label">MAKE Studio</p>
-		<img src="/images/bcssn-logo-color.png" alt="Baltimore County Student Support Network logo" className="logo__charity" />
-		<p className="registry__label">Baltimore County Student Support Network</p>
+		<div className="charity__block-left">
+			<div className="logo__block">
+				<img src="/images/wwp_logo_black.png" alt="Wounded Warrior Project logo" className="logo__charity" />
+				<p className="registry__label">Wounded Warrior Project</p>
+			</div>
+			<p className="description">
+				Block of text describing charity. Block of text describing charity. Block of text describing charity. Block of text describing
+				charity. Block of text describing charity. Block of text describing charity. Block of text describing charity. Block of text
+				describing charity.
+			</p>
+		</div>
+		<div className="divider" />
+		<div className="charity__block-right">
+			<div className="logo__block">
+				<img src="/images/make-logo.jpeg" alt="MAKE Studio logo" className="logo__charity-large" />
+				<p className="registry__label">MAKE Studio</p>
+			</div>
+			<p className="description">
+				Block of text describing charity. Block of text describing charity. Block of text describing charity. Block of text describing
+				charity. Block of text describing charity. Block of text describing charity. Block of text describing charity. Block of text
+				describing charity.
+			</p>
+		</div>
+		<div className="divider" />
+		<div className="charity__block-left">
+			<div className="logo__block">
+				<img src="/images/bcssn-logo-color.png" alt="Baltimore County Student Support Network logo" className="logo__charity-large" />
+				<p className="registry__label">Baltimore County Student Support Network</p>
+			</div>
+			<p className="description">
+				Block of text describing charity. Block of text describing charity. Block of text describing charity. Block of text describing
+				charity. Block of text describing charity. Block of text describing charity. Block of text describing charity. Block of text
+				describing charity.
+			</p>
+		</div>
+		<div className="divider" />
+		<div className="charity__block-right">
+			<div className="logo__block">
+				<img src="/images/CatholicCharities.png" alt="Maryland Catholic Charities logo" className="logo__charity" />
+				<p className="registry__label">Catholic Charities of Maryland</p>
+			</div>
+			<p className="description">
+				Block of text describing charity. Block of text describing charity. Block of text describing charity. Block of text describing
+				charity. Block of text describing charity. Block of text describing charity. Block of text describing charity. Block of text
+				describing charity.
+			</p>
+		</div>
+		<div className="divider" />
+		<a href="https://www.honeyfund.com/OurRegistry">
+			<div className="charity__block-left">
+				<div className="logo__block">
+					<img src="/images/honeyfund-logo.svg" alt="Honeyfund logo" className="logo__charity" />
+					<p className="registry__label">Honeymoon in Finland</p>
+				</div>
+				<p className="description">
+					Block of text describing honeymoon & honeyfund. Block of text describing honeymoon & honeyfund. Block of text describing honeymoon
+					& honeyfund. Block of text describing honeymoon & honeyfund. Block of text describing honeymoon & honeyfund. Block of text
+					describing honeymoon & honeyfund.
+				</p>
+			</div>
+		</a>
 	</section>
 );
 
