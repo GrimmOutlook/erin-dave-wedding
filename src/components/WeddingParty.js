0a74872e2af382b86a236c4a48380295acc699a8
GrimmOutlook <grimmoutlook87@gmail.com>
2019-06-02 11:37:35 -0400

    WeddingParty section done & with reusable component.

--- src/components/WeddingParty.js
+++ src/components/WeddingParty.js
@@ -2,46 +2,22 @@ import React from 'react';
 
 import './WeddingParty.css';
 
-const WeddingParty = () => (
+const WeddingParty = props => (
 	<section className="bridesmaid" id="weddingparty">
-		<h1>Bridesmaids</h1>
+		<h1>{props.weddingParty[0].id === 1 ? 'Bridesmaids' : 'Groomsmen'}</h1>
 		<div className="bridesmaid__grid">
-			<div className="wrapper__bridesmaid">
-				<div className="wrapper__image">
-					<img src="/images/erin-misty.JPG" alt="Misty Maid-of-Honor" className="photo__maid1" />
-				</div>
-				<div className="personal__info">
-					<div className="name">Misty Cantwell</div>
-					<div className="title">Maid of Honor</div>
-					<div className="bio">
-						block of text about person. block of text about person. block of text about person. block of text about person.
+			{props.weddingParty.map(item => (
+				<div className="wrapper__bridesmaid">
+					<div className="wrapper__image">
+						<img src={item.mainImage} alt={item.alt} className={item.hoverImageClass} />
+					</div>
+					<div className="personal__info">
+						<div className="name">{item.name}</div>
+						<div className="title">{item.title}</div>
+						<div className="bio">{item.bio}</div>
 					</div>
 				</div>
-			</div>
-			<div className="wrapper__bridesmaid">
-				<div className="wrapper__image">
-					<img src="/images/erin-cristina2.jpg" alt="Erin Cristina" className="photo__maid2" />
-				</div>
-				<div className="personal__info">
-					<div className="name">Cristina Snow</div>
-					<div className="title">Bridesmaid</div>
-					<div className="bio">
-						block of text about person. block of text about person. block of text about person. block of text about person.
-					</div>
-				</div>
-			</div>
-			<div className="wrapper__bridesmaid">
-				<div className="wrapper__image">
-					<img src="/images/erin-claire.jpg" alt="Erin Claire" className="photo__maid3" />
-				</div>
-				<div className="personal__info">
-					<div className="name">Claire MacNamara</div>
-					<div className="title">Bridesmaid</div>
-					<div className="bio">
-						block of text about person. block of text about person. block of text about person. block of text about person.
-					</div>
-				</div>
-			</div>
+			))}
 		</div>
 	</section>
 );
--- src/index.js
+++ src/index.js
@@ -12,32 +12,77 @@ import Registry from './components/Registry';
 
 import './mystyles.scss';
 
+const BRIDESMAIDS = [
+	{
+		id: 1,
+		name: 'Misty Cantwell',
+		title: 'Maid of Honor',
+		bio: 'block of text about person. block of text about person. block of text about person. block of text about person.',
+		mainImage: '/images/erin-misty.JPG',
+		alt: 'Misty - Maid of Honor',
+		hoverImageClass: 'photo__maid1',
+	},
+	{
+		id: 2,
+		name: 'Cristina Snow',
+		title: 'Bridesmaid',
+		bio: 'block of text about person. block of text about person. block of text about person. block of text about person.',
+		mainImage: '/images/erin-cristina2.jpg',
+		alt: 'Cristina - Bridesmaid',
+		hoverImageClass: 'photo__maid2',
+	},
+	{
+		id: 3,
+		name: 'Claire MacNamara',
+		title: 'Bridesmaid',
+		bio: 'block of text about person. block of text about person. block of text about person. block of text about person.',
+		mainImage: '/images/erin-claire.jpg',
+		alt: 'Claire - Bridesmaid',
+		hoverImageClass: 'photo__maid3',
+	},
+];
+
+const GROOMSMEN = [
+	{
+		id: 4,
+		name: 'Aaron Stearns',
+		title: 'Best Man',
+		bio: 'block of text about person. block of text about person. block of text about person. block of text about person.',
+		mainImage: '/images/dave-aaron.jpg',
+		alt: 'Aaron - Best Man',
+		hoverImageClass: 'photo__groomsman1',
+	},
+	{
+		id: 5,
+		name: 'Scott Scherer',
+		title: 'Groomsman',
+		bio: 'block of text about person. block of text about person. block of text about person. block of text about person.',
+		mainImage: '/images/dave-olympic (1).jpg',
+		alt: 'Scott - Groomsman',
+		hoverImageClass: 'photo__groomsman2',
+	},
+	{
+		id: 6,
+		name: 'Sean Curry',
+		title: 'Groomsman',
+		bio: 'block of text about person. block of text about person. block of text about person. block of text about person.',
+		mainImage: '/images/dave-sean.jpg',
+		alt: 'Sean - Groomsman',
+		hoverImageClass: 'photo__groomsman3',
+	},
+];
+
 const App = () => (
 	<React.Fragment>
 		<Nav />
 		<Hero />
 		<BrideGroom />
-		<WeddingParty />
+		<WeddingParty weddingParty={BRIDESMAIDS} />
+		<WeddingParty weddingParty={GROOMSMEN} />
 		<WhenWhere />
 		<Lodging />
 		<RSVP />
 		<Registry />
-		{/* <div className="flex__container">
-			<div className="save__date2" />
-			<div className="save__date">
-				<div className="save__date__text">
-					<h2>Save The</h2>
-					<h1 className="fancy__font">
-						<span className="font__alt">d</span>at<span className="font__alt">E</span>
-						<span className="fancy__font">Erin & Dave</span>
-					</h1>
-					<h2>September 7, 2019</h2>
-					<h3>Columbia, Maryland</h3>
-					<h3>More Details to Follow!</h3>
-				</div>
-			</div>
-		</div> */}
-		{/* <p>Wedding Stuff</p> */}
 	</React.Fragment>
 );
 
