ed4887e6fb28a5425e66a3878b38cbc2ab1fcce7
GrimmOutlook <grimmoutlook87@gmail.com>
2019-06-01 22:53:11 -0400

    Bridesmaid section mostly complete. Again.

--- src/components/BrideGroom.js
+++ src/components/BrideGroom.js
@@ -10,13 +10,7 @@ const BrideGroom = () => (
 				{/* <div className="photo__bride" /> */}
 				<div className="container__bride__photo">
 					<div className="wrapper__bride">
-						<img src="/images/erin-boat.jpg" alt="Erin boat" className="photo__bride__dt" />
-					</div>
-					<div className="wrapper__bride">
 						<img src="/images/erin-primantis.jpg" alt="Erin Primantis" className="photo__bride__dt" />
-					</div>
-					<div className="wrapper__bride">
-						<img src="/images/erin-vegas.jpg" alt="Erin purple zebra" className="photo__bride__dt" />
 					</div>
 				</div>
 			</div>
@@ -25,12 +19,6 @@ const BrideGroom = () => (
 				<div className="container__bride__photo">
 					<div className="wrapper__bride">
 						<img src="/images/dave-olympic (1).jpg" alt="Dave Olympic Mtns" className="photo__groom__dt1" />
-					</div>
-					<div className="wrapper__bride">
-						<img src="/images/dave-boat.jpg" alt="Dave on a boat" className="photo__groom__dt2" />
-					</div>
-					<div className="wrapper__bride">
-						<img src="/images/erin-vegas.jpg" alt="Erin purple zebra" className="photo__bride__dt" />
 					</div>
 				</div>
 			</div>
--- src/components/WeddingParty.js
+++ src/components/WeddingParty.js
@@ -7,7 +7,9 @@ const WeddingParty = () => (
 		<h1>Bridesmaids</h1>
 		<div className="bridesmaid__grid">
 			<div className="wrapper__bridesmaid">
-				<img src="/images/erin-misty.JPG" alt="Misty Maid-of-Honor" className="photo__maid1" />
+				<div className="wrapper__image">
+					<img src="/images/erin-misty.JPG" alt="Misty Maid-of-Honor" className="photo__maid1" />
+				</div>
 				<div className="personal__info">
 					<div className="name">Misty Cantwell</div>
 					<div className="title">Maid of Honor</div>
@@ -17,7 +19,9 @@ const WeddingParty = () => (
 				</div>
 			</div>
 			<div className="wrapper__bridesmaid">
-				<img src="/images/erin-cristina2.jpg" alt="Erin Cristina" className="photo__maid2" />
+				<div className="wrapper__image">
+					<img src="/images/erin-cristina2.jpg" alt="Erin Cristina" className="photo__maid2" />
+				</div>
 				<div className="personal__info">
 					<div className="name">Cristina Snow</div>
 					<div className="title">Bridesmaid</div>
@@ -27,7 +31,9 @@ const WeddingParty = () => (
 				</div>
 			</div>
 			<div className="wrapper__bridesmaid">
-				<img src="/images/erin-claire.jpg" alt="Erin Claire" className="photo__maid3" />
+				<div className="wrapper__image">
+					<img src="/images/erin-claire.jpg" alt="Erin Claire" className="photo__maid3" />
+				</div>
 				<div className="personal__info">
 					<div className="name">Claire MacNamara</div>
 					<div className="title">Bridesmaid</div>
